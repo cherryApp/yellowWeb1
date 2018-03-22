0ba89383ec4bda8fc875b82e3eb83a325e54ab54
Szász Kinga <kingus77@gmail.com>
2018-03-22 21:57:10 +0100

    rounds listed out

--- src/app/focikinga/focikinga.component.ts
+++ src/app/focikinga/focikinga.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { HttpClient } from "@angular/common/http";
+import { ClassGetter } from "@angular/compiler/src/output/output_ast";
 
 @Component({
   selector: 'app-focikinga',
@@ -6,10 +9,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./focikinga.component.css']
 })
 export class FocikingaComponent implements OnInit {
+  rounds: Array<matchday>;
+  day: any;
 
-  constructor() { }
+  jsonUrl: string = "https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/en.1.json";
+  lastKey: string = '';
+  multiplier: number = 1;
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.jsonUrl).subscribe((data: jsonData) => {
+      console.log(data);
+      this.rounds = data.rounds;
+    })
+  }
 
   ngOnInit() {
   }
 
+  showMatchday(day: any) {
+    this.rounds[day] = day;
+    console.log(this.rounds[day]);
+  }
 }
+
+interface jsonData {
+  name: string;
+  rounds: Array<matchday>;
+}
+
+interface matchday {
+  name: string;
+  matches: Array<match>;
+}
+
+interface match {
+  date: Date;
+  team1: team;
+  team2: team;
+  score1: number;
+  score2: number;
+}
+
+interface team {
+  key: string;
+  name: string;
+  code: string;
+}
+
