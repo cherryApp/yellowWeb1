1e7b08432c7e3bad13a9b0c209e0bb7932e7b3f6
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-03-22 15:56:39 +0100

    first

--- src/app/weather04/weather04.component.ts
+++ src/app/weather04/weather04.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+import { HttpClient } from '@angular/common/http';
+import { ClassGetter } from "@angular/compiler/src/output/output_ast";
 
 @Component({
   selector: 'app-weather04',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather04.component.css']
 })
 export class Weather04Component implements OnInit {
+  dataBud: Array<any> = [];
+  jsonUrl1: string = "http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=d02ce6adf455a12b46649daf11169961";
+  jsonUrl2: string = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=d02ce6adf455a12b46649daf11169961";
+  jsonUrl3: string = "http://api.openweathermap.org/data/2.5/weather?q=New%20York,us&appid=d02ce6adf455a12b46649daf11169961";
+  jsonUrl4: string = "http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=d02ce6adf455a12b46649daf11169961";
+  jsonUrl5: string = "http://api.openweathermap.org/data/2.5/weather?q=Rome,it&appid=d02ce6adf455a12b46649daf11169961";
+  constructor(private http: HttpClient) {
+    this.http.get(this.jsonUrl1).subscribe((data: serverData) => {
+      console.log(data);
+      this.dataBud.push(data);
+      console.log(this.dataBud[0].name)
+    });
 
-  constructor() { }
+  }
 
   ngOnInit() {
   }
-
 }
+  interface serverData {
+    name: string;
+    dataBud: Array<any>;
+
+  }
+
