380920726181d2e54e7b6327b05425133565e1f4
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-03-25 20:53:29 +0200

    ok

--- src/app/weather04/weather04.component.ts
+++ src/app/weather04/weather04.component.ts
@@ -9,42 +9,33 @@ import { ClassGetter } from "@angular/compiler/src/output/output_ast";
   styleUrls: ['./weather04.component.css']
 })
 export class Weather04Component implements OnInit {
-  dataBud: Array<any> = [];
-  jsonUrl1: string = "http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=d02ce6adf455a12b46649daf11169961";
-  jsonUrl2: string = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=d02ce6adf455a12b46649daf11169961";
-  jsonUrl3: string = "http://api.openweathermap.org/data/2.5/weather?q=New%20York,us&appid=d02ce6adf455a12b46649daf11169961";
-  jsonUrl4: string = "http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&appid=d02ce6adf455a12b46649daf11169961";
-  jsonUrl5: string = "http://api.openweathermap.org/data/2.5/weather?q=Rome,it&appid=d02ce6adf455a12b46649daf11169961";
+  urlBeginning: string = "http://api.openweathermap.org/data/2.5/weather?q=";
+  urlEnding: string = "&appid=d02ce6adf455a12b46649daf11169961";
+  dataDisplay: Array<any> = [];
+  weatherData: Array<any> =
+    ["Budapest,hu",
+      "London,uk",
+      "New%20York,us",
+      "Paris,fr",
+      "Rome,it"];
+
   constructor(private http: HttpClient) {
-    this.http.get(this.jsonUrl1).subscribe((data: serverData) => {
-      console.log(data);
-      this.dataBud.push(data);
-    });
-    this.http.get(this.jsonUrl2).subscribe((data: serverData) => {
-     
-      this.dataBud.push(data);
-    });
-    this.http.get(this.jsonUrl3).subscribe((data: serverData) => {
-     
-      this.dataBud.push(data);
-    });
-    this.http.get(this.jsonUrl4).subscribe((data: serverData) => {
-     
-      this.dataBud.push(data);
-    });
-    this.http.get(this.jsonUrl5).subscribe((data: serverData) => {
-     
-      this.dataBud.push(data);
-    });
+    for (let i = 0; i < this.weatherData.length; i++) {
+      this.http.get(this.urlBeginning + this.weatherData[i] + this.urlEnding).subscribe((data: serverData) => {
+        console.log(data);
+        this.dataDisplay.push(data);
+      });
+    }
+
 
   }
 
   ngOnInit() {
   }
 }
-  interface serverData {
-    name: string;
-    dataBud: Array<any>;
+interface serverData {
+  name: string;
+  dataDisplay: Array<any>;
 
-  }
+}
 
