661fac93a215957c37e426df1d25b87c9e34327e
Targaryen57 <marton.rubi@gmail.com>
2018-03-26 00:35:41 +0200

    one city shown

--- src/app/weather05/weather05.component.ts
+++ src/app/weather05/weather05.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from "@angular/http";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-weather05',
@@ -6,10 +8,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather05.component.css']
 })
 export class Weather05Component implements OnInit {
+  weather: Array<any> = [];
+  city: String = "";
+  temp: Number = 0;
+  pressure: Number = 0;
+  humidity: Number = 0;
 
-  constructor() { }
+  jsonUrl: string = "http://api.openweathermap.org/data/2.5/weather?q=Budapest,hu&appid=7b589666541dced92f6bb1ae15152055";
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.jsonUrl).subscribe((data: serverData) => {
+      this.city = data.name;
+      this.temp = parseFloat(data.main.temp) - 273.15;
+      this.temp = this.temp.toFixed(2)
+      this.pressure = data.main.pressure;
+      this.humidity = data.main.humidity;
+      this.weather.push(data.weather[0].main);
+      this.weather.push(data.weather[0].description);
+
+      console.log(data);
+      console.log(this.city);
+      console.log(this.weather);
+    });
+  }
 
   ngOnInit() {
   }
+
+}
+
+interface serverData {
+  name: string;
+  coord: object;
+  main: object;
+
 
 }
