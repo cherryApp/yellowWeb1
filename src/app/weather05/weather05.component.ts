dff2269753299eec2d6ea9b590f72ecb8cd3b7f4
Targaryen57 <marton.rubi@gmail.com>
2018-03-26 06:28:08 +0200

    professional help works

--- src/app/weather05/weather05.component.ts
+++ src/app/weather05/weather05.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 import { Http } from "@angular/http";
 import { HttpClient } from "@angular/common/http";
 import { NgModel } from '@angular/forms';
@@ -8,7 +8,10 @@ import { NgModel } from '@angular/forms';
   templateUrl: './weather05.component.html',
   styleUrls: ['./weather05.component.css']
 })
+
+
 export class Weather05Component implements OnInit {
+  sziliShow: boolean = true;
   weather: Array<any> = [];
   city: String = "";
   temp: Number = 0;
@@ -16,16 +19,16 @@ export class Weather05Component implements OnInit {
   humidity: Number = 0;
   icon: String = "";
   iconArray: Array<any> = [];
-  citySearch: String;
-  countrySearch: String;
-
-  console.log(citySearch);
-
+  citySearch: String = "Budapest";
+  countrySearch: String = "Hungary";
   jsonUrl: string = "http://api.openweathermap.org/data/2.5/weather?q=Chicago,us&appid=7b589666541dced92f6bb1ae15152055";
   iconJsonUrl: string = "https://gist.githubusercontent.com/tbranyen/62d974681dea8ee0caa1/raw/3405bfb2a76b7cbd90fde33d8536f0cd13706955/icons.json";
 
+
   constructor(private http: HttpClient) {
 
+    this.jsonUrl = this.onSearch(this.citySearch, this.countrySearch, this.http);
+    console.log(this.jsonUrl);
 
     this.http.get(this.jsonUrl).subscribe((data: serverData) => {
       this.city = data.name;
@@ -37,64 +40,63 @@ export class Weather05Component implements OnInit {
       this.weather.push(data.weather[0].description);
 
 
-
       this.http.get(this.iconJsonUrl).subscribe((data: iconData) => {
         this.iconArray = Object.keys(data).map(i => data[i]);
 
         //egyszerubb lett volna csak a stringet osszehasonlitani, de igy egy weather modult importalva erikFlowers gitrol egybol jo ikont illeszt be
         switch (this.whatIcon(this.weather[1], this.iconArray)) {
           case 'storm-showers':
-            console.log('storm-showers');
+
             this.icon = 'assets/weatherIcons/drop.png'
             break;
           case 'thunderstorm':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/light.png'
             break;
           case 'sprinkle':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/drop.png'
             break;
           case 'rain':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/drop.png'
             break;
           case 'showers':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/drop.png'
             break;
           case 'snow':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/snowflake.png'
             break;
           case 'day-haze':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/sunny.png'
             break;
           case 'cloudy-gusts':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/cloudy.png'
             break;
           case 'hail':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/snowflake.png'
             break;
           case 'hot':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/sunny.png'
             break;
           case 'sunny':
-            console.log('thunderstorm');
+
             this.icon = 'assets/weatherIcons/sunny.png'
             break;
           case 'cloudy':
-            console.log('cloudy');
+
             this.icon = 'assets/weatherIcons/cloud.png'
             break;
           default:
             this.icon = 'assets/weatherIcons/sad.png'
         }
-c
+
 
 
 
@@ -125,19 +127,32 @@ c
 
   }
 
-  onSearch(citySearch, countrySearch){
-    
+  onSearch(citySearch, countrySearch, http) {
+    this.citySearch = this.citySearch.toLowerCase();
+    this.citySearch = this.citySearch.charAt(0).toUpperCase() + this.citySearch.slice(1);
+    this.countrySearch = this.countrySearch.substring(0, 2).toLowerCase();
+    console.log(this.citySearch, this.countrySearch);
 
+
+    return `http://api.openweathermap.org/data/2.5/weather?q=${this.citySearch},${this.countrySearch}&appid=7b589666541dced92f6bb1ae15152055`;
+  }
+
+  sziliClick() {
+    this.sziliShow = !this.sziliShow;
   }
 
 
 
 }
 
+
+
+
 interface serverData {
   name: string;
   coord: object;
   main: object;
+
 }
 
 
