d72936400376a8c8124dbf4347a0b6dc6b11e793
Szász Kinga <kingus77@gmail.com>
2018-03-26 09:12:18 +0200

    last

--- src/app/focikinga/focikinga.component.ts
+++ src/app/focikinga/focikinga.component.ts
@@ -3,6 +3,7 @@ import { Http } from '@angular/http';
 import { HttpClient } from "@angular/common/http";
 import { ClassGetter } from "@angular/compiler/src/output/output_ast";
 import { JsonData, Matchday, Match } from './data';
+import { isNullOrUndefined } from 'util';
 
 @Component({
   selector: 'app-focikinga',
@@ -12,24 +13,66 @@ import { JsonData, Matchday, Match } from './data';
 export class FocikingaComponent implements OnInit {
   rounds: Array<Matchday>;
   matchday: Matchday;
-
   jsonUrl: string = "https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/en.1.json";
+  lastKey: string = '';
+  multiplier: number = 1;
+
+  clubs2: Array<any> = [];
+  jsonUrl2: string = "https://raw.githubusercontent.com/opendatajson/football.json/master/2016-17/es.1.clubs.json";
 
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private http2: HttpClient) {
     this.http.get(this.jsonUrl).subscribe((data: JsonData) => {
-      console.log(data);
+      // console.log(data);
       this.rounds = data.rounds;
       this.matchday = this.rounds[0];
-    })
+      console.log(this.rounds[0]);
+    });
+
+    this.http2.get(this.jsonUrl2).subscribe((data2: serverData) => {
+      this.clubs2 = data2.clubs;
+      //console.log(data2);
+    });
+
   }
 
   ngOnInit() {
   }
 
-  showMatchday(day: any) {
-    this.rounds[day] = day;
-    console.log(this.rounds[day]);
-
+  showMatchday(day: any, k: number) {
+    this.rounds[k] = day;
+    console.log(this.rounds[k]);
   }
+
+
+  updateRow(i: number, club: any) {
+    console.log(club);
+    console.log(i);
+  }
+
+  deleteRow(i) {
+    if (confirm('Really?')) {
+      this.clubs2.splice(i, 1);
+    }
+  }
+
+  sortTable(key: string) {
+
+    if (this.lastKey == key) {
+      this.multiplier *= -1;
+    }
+    this.clubs2.sort((a, b) => {
+      !a[key] ? a[key] = "" : a[key];
+      !b[key] ? b[key] = "" : b[key];
+
+      return a[key].localeCompare(b[key]) * this.multiplier;
+    });
+    this.lastKey = key;
+  }
+}
+
+
+interface serverData {
+  name: string;
+  clubs: Array<any>;
 }
--- src/app/focikinga/selected-match/selected-match.component.ts
+++ src/app/focikinga/selected-match/selected-match.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FocikingaComponent } from '../focikinga.component';
-import { Matchday } from '../data';
+import { Matchday, JsonData } from '../data';
 
 @Component({
   selector: 'app-selected-match',
   templateUrl: './selected-match.component.html',
   styleUrls: ['./selected-match.component.css']
 })
+
 export class SelectedMatchComponent implements OnInit {
   @Input() day: Matchday;
-
+  @Input() k: number;
+  @Input() rounds: JsonData;
   constructor() {
+
   }
 
   ngOnInit() {
