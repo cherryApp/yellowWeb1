7a9f3f5426bda01f9b53ff0862b3b4726966126d
Doni Pataki <yellow.pataki.doni@gmail.com>
2018-03-26 09:11:05 +0200

    monday

--- src/app/weather04/weather04.component.ts
+++ src/app/weather04/weather04.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { Http } from "@angular/http";
 import { HttpClient } from '@angular/common/http';
 import { ClassGetter } from "@angular/compiler/src/output/output_ast";
+import { Directive, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-weather04',
   templateUrl: './weather04.component.html',
   styleUrls: ['./weather04.component.css']
 })
+
+
 export class Weather04Component implements OnInit {
   urlBeginning: string = "http://api.openweathermap.org/data/2.5/weather?q=";
   urlEnding: string = "&appid=d02ce6adf455a12b46649daf11169961";
@@ -17,7 +20,8 @@ export class Weather04Component implements OnInit {
       "London,uk",
       "New%20York,us",
       "Paris,fr",
-      "Rome,it"];
+      "Rome,it",
+      "Sydney,au"];
 
   constructor(private http: HttpClient) {
     for (let i = 0; i < this.weatherData.length; i++) {
@@ -25,6 +29,7 @@ export class Weather04Component implements OnInit {
         console.log(data);
         this.dataDisplay.push(data);
       });
+      this.dataDisplay.sort();
     }
 
 
@@ -32,6 +37,8 @@ export class Weather04Component implements OnInit {
 
   ngOnInit() {
   }
+
+
 }
 interface serverData {
   name: string;
