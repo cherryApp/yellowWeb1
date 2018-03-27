230904c58e66f89c2c5c3f1ce0ff12a4d4896344
Oberth Zsigmond <oberthzsigmond@gmail.com>
2018-03-27 15:34:28 +0200

    zsiga

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,24 +8,25 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   showDropdown: boolean = false;
 
-  menuList: Array<{label: string, url: string}> = [
-    {label: 'Péti', url: '/soccer1'},
-    {label: 'Tomi', url: '/weather01'},
-    {label: 'Kinga', url: '/focikinga'},
-    {label: 'Réka', url: '/focireka'},
-    {label: 'Andi', url: '/fociandi'},
-    {label: 'Olivér', url: '/olikaja'},
-    {label: 'Miro', url: '/soccer02'},
-    {label: 'Peti', url: '/weather02'},
-    {label: 'Frédi', url: '/weather03'},
-    {label: 'Linda', url: '/lindakaja'},
-    {label: 'Doni', url: '/weather04'},
-    {label: 'Máté', url: '/matekaja'},
-    {label: 'Ricsi', url: '/fociricsi'},
-    {label: 'Marci', url: '/weather05'},
-    {label: 'Péter', url: '/weather06'},
-    {label: 'Zsombor', url: '/weather07'},
-    {label: 'Balázs', url: '/focibalazs'}
+  menuList: Array<{ label: string, url: string }> = [
+    { label: 'Péti', url: '/soccer1' },
+    { label: 'Tomi', url: '/weather01' },
+    { label: 'Kinga', url: '/focikinga' },
+    { label: 'Réka', url: '/focireka' },
+    { label: 'Andi', url: '/fociandi' },
+    { label: 'Olivér', url: '/olikaja' },
+    { label: 'Miro', url: '/soccer02' },
+    { label: 'Peti', url: '/weather02' },
+    { label: 'Frédi', url: '/weather03' },
+    { label: 'Linda', url: '/lindakaja' },
+    { label: 'Doni', url: '/weather04' },
+    { label: 'Máté', url: '/matekaja' },
+    { label: 'Ricsi', url: '/fociricsi' },
+    { label: 'Marci', url: '/weather05' },
+    { label: 'Péter', url: '/weather06' },
+    { label: 'Zsombor', url: '/weather07' },
+    { label: 'Balázs', url: '/focibalazs' }
+    , { label: 'Zsiga', url: '/focizsiga' }
   ];
   constructor() { }
 
